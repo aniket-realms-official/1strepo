58b5cdd98017d09f57799ddc3e35c57e143c0632
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-11 18:43:32 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-2"
   access_key = "AKIATC4BRTUBLS27AOXO"
-  secret_key = "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
+  secret_key = "h7NzFBKv00MD2v//kKL/NbMeY3p4d9zlGqQuRlq5"
 "
 }
