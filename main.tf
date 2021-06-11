52bbac0fbd49dab38a99d24933029951796834a8
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-11 18:42:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "myfirstEC2" {
   instance_type          = var.ec2_instance_type
   count                  = var.ec2_count
   tags                   = var.ec2_tags
-  vpc_security_group_ids = var.sg_id
-  subnet_id              = var.subnet_public_external_id
+#  vpc_security_group_ids = var.sg_id
+#  subnet_id              = var.subnet_public_external_id
 }
