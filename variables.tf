ccf36e78aa46224fd0067f192230e4467cfaed73
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-11 18:25:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,7 +8,6 @@ variable "ec2_instance_type" {
   type        = string
   default     = "t2.micro"
 }
-
 variable "ec2_tags" {
   description = "Tags to apply to resources created by ec2 module"
   type        = map(string)
@@ -18,18 +17,15 @@ variable "ec2_tags" {
     Name               = "AppServer"
   }
 }
-
 variable "ec2_count" {
   description = "Define number of instance required"
   type        = number
   default     = "1"
 }
-
 variable "sg_id" {
   type = string
   default = ["${aws_security_group.sg_01.id}"]
 }
-
 variable "subnet_public_external_id" {
   type = string
   default = aws_subnet.subnet_public_external.id
