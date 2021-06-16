fa40722dffe56efed2159db6ae4e714f64b50131
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-16 19:21:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "vpc" {
 module "private_subnet" {
   source = "./networking/subnet/private" #Project path: networking\subnet\private
   vpc_id = module.vpc.vpc_id
+  depends_on = ["module.aws_provider"]
 }
 
 #####################################################
@@ -21,6 +22,7 @@ module "private_subnet" {
 module "public_subnet" {
   source = "./networking/subnet/public" #Project path: networking\subnet\public
   vpc_id = module.vpc.vpc_id
+  depends_on = ["module.aws_provider"]
 }
 
 # #######################################################
@@ -88,3 +90,7 @@ module "ec2" {
 #   tls_private_key.myec2keypair
 #  ]
 #  }
+module "aws_provider" {
+  source = "./providers.tf
+  region = "${var.region}"
+}
