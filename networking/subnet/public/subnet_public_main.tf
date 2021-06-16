cb7acc99a98c98ac767bd3d360a14c6f94a78379
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-16 19:37:41 +0530

    Update subnet_public_main.tf

--- networking/subnet/public/subnet_public_main.tf
+++ networking/subnet/public/subnet_public_main.tf
@@ -4,7 +4,7 @@
 resource "aws_subnet" "subnet_public" {
   vpc_id                  = var.vpc_id
   cidr_block              = var.pub_cidr_block
-  availability_zone       = var.region
+  availability_zone       = var.pub_az
   map_public_ip_on_launch = var.map_public_ip
   tags = var.public_subnet__tags
 }
