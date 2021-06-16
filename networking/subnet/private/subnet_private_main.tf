ad1c484ccf06e1a76fe2963d6a7216e30db8e5e3
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-16 18:11:20 +0530

    Update subnet_private_main.tf

--- networking/subnet/private/subnet_private_main.tf
+++ networking/subnet/private/subnet_private_main.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "subnet_private" {
   vpc_id            = var.vpc_id
   cidr_block        = var.pri_sub_cidr_block
-  availability_zone = var.region
+  availability_zone = var.pri_sub_availability_zone
   tags              = var.pri_sub_tags
+  region            = var.region
 }
