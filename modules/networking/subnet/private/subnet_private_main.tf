3fb38f2c7da1782a6516c093bad0a9c472699a68
aniket khedekar <aniket.khedekar@newvisionsoftpune.com>
2021-06-18 16:06:43 +0530

    test

--- modules/networking/subnet/private/subnet_private_main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_subnet" "subnet_private" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.pri_sub_cidr_block
-  availability_zone = var.pri_sub_availability_zone
-  tags              = var.pri_sub_tags
-}
