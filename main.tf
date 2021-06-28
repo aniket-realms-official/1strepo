bf03652a11b768d5d5d7d7b00181626db3ce2cd4
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-28 13:28:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "private_subnet" {
 module "public_subnet" {
   source = "./networking/subnet/public" #Project path: networking\subnet\public
   vpc_id = module.vpc.vpc_id
-  pub_az = "${var.pub_az}"
+  #pub_az = "${var.pub_az}"
 }
 
 # #######################################################
