51337197224cb74c6958e914cccda56804a72ec0
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-28 13:39:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 module "private_subnet" {
   source = "./networking/subnet/private" #Project path: networking\subnet\private
   vpc_id = module.vpc.vpc_id
-  availability_zone = "${var.pri_sub_availability_zone}"
+  availability_zone = var.pri_sub_availability_zone
 }
 
 #####################################################
@@ -22,7 +22,7 @@ module "private_subnet" {
 module "public_subnet" {
   source = "./networking/subnet/public" #Project path: networking\subnet\public
   vpc_id = module.vpc.vpc_id
-  availability_zone = "${var.pub_az}"
+  availability_zone = var.pub_az
 }
 
 # #######################################################
