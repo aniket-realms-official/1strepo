d04e126c83e50ce319078f86c64c2507e5edb15a
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-28 14:35:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,1 @@
-variable "pri_sub_availability_zone" {
-  description = ""
-  type        = string
-  default     = "us-west-1b"
-}
-variable "pub_az" {
-  description = ""
-  type        = string
-  default     = "us-west-1a"
-}
+
