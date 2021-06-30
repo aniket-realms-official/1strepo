d2f2a220bffd3b477b2df0e00f66a75097f99f8c
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-06-30 17:49:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,3 @@ variable "public_az" {
 variable "private_az" {
   default = ""
 }
-variable "region" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "us-west-1"
-}
