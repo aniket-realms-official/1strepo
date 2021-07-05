70c87e726196506061a5d81505a71cb49ea2ddf2
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-07-05 13:50:42 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myfirstEC2" {
-  ami                = var.ec2_ami
-  instance_type          = var.ec2_instance_type
-  count                  = var.ec2_count
-  tags                   = var.ec2_tags
-#  vpc_security_group_ids = var.sg_id
-#  subnet_id              = var.subnet_public_external_id
-}
