6b39d714671719adcae072f4b7ed82195a1f9d3c
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-07-05 16:46:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "Use AWS region"
   type        = string
 }
+variable "ssh_public_key_file" {
+  # Generate via 'ssh-keygen -f openvpn -t rsa'
+  description = "The public SSH key to store in the EC2 instance"
+  default     = "settings/pem-key"
+}
