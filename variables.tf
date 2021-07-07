cbe602233fb75d998bd0898abd4272c1f1ec55b6
aniket-realm <50407594+aniket-realm@users.noreply.github.com>
2021-07-07 18:06:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "region" {
   description = "Use AWS region"
   type        = string
 }
+variable "ec2_count" {
+default = "2"
+}
+variable "ebs_vol_size" {
+default = "10"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
