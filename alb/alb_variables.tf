de8a73e02b41bb4a38a0028cf1c461d2ee2d6710
Your Name <you@example.com>
2021-07-21 14:44:09 +0530

    removed code

--- alb/alb_variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {}
-
-variable "subnet1" {}
-
-variable "subnet2" {}
-
-variable "inst1_id" {}
-
-variable "inst2_id" {}
--- autoscaling/autoscaling_variable.tf
+++
@@ -1,6 +0,0 @@
-variable "target_group_arn" {}
-
-variable "subnet1" {}
-variable "subnet2" {}
-variable "vpc_id" {}
-variable "ec2_value_name" {}
